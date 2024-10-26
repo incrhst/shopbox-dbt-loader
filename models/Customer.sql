6f1972110bfe09766fa2a8224f559e5a50356054
David Bain <pigeonflight@gmail.com>
2024-10-26 09:06:28 -0500

    fix on Customer model

--- models/Customer.sql
+++ models/Customer.sql
@@ -69,7 +69,7 @@ WITH source_customers AS (
 existing_customers AS (
     SELECT
         CustomerAccountNumber,
-        CustomerAgentPrefix,
+        AgentPrefix as CustomerAgentPrefix,
         '' AS CustomerReference  -- Match the source column with COALESCE
     FROM {{ source('migration', 'Customer') }}
 ),
