ee95854d36993f19f72c537c95edae9db993602d
incrhst <110049091+incrhst@users.noreply.github.com>
2024-10-25 20:39:13 -0500

    Update ImportPackage.sql

--- models/ImportPackage.sql
+++ models/ImportPackage.sql
@@ -7,7 +7,7 @@
 {% set migration_schema = 'migration' %}
 {% set colsql = 'COLLATE Modern_Spanish_CI_AS' %}
 
-WITH source AS (
+WITH list_of_existing_package_numbers AS (
     -- Start with ImportPackage to ensure we only process valid packages
     SELECT DISTINCT p.PackageNumber {{ colsql }} as PackageNumber
     FROM {{ source(migration_schema, 'ImportPackage') }} p
@@ -27,14 +27,14 @@ existing_packages AS (
     {% if is_incremental() %}
     SELECT PackageNumber FROM {{ this }}
     {% else %}
-    SELECT PackageNumber FROM {{ target.schema }}.Package
+    SELECT NULL as PackageNumber WHERE 1=0  -- Empty result on first run since nothing exists yet
     {% endif %}
 ),
 transformed AS (
     SELECT
         pd.PackageNumber {{ colsql }} AS PackageNumber,
         pd.InternationalTrackingNumber {{ colsql }} AS PackageInternationalTrackingNu,
-        COALESCE(pd.PrealertTrackingNumber, '') AS PackagePrealertTrackingNumber,
+        COALESCE(pd.prealert_tracking_number, '') AS PackagePrealertTrackingNumber,
         COALESCE(pd.SupplierInvoiceNumber, '') AS PackageSupplierInvoiceNumber,
         pd.PackageDescription {{ colsql }} AS PackageDescription,
         pd.TariffDescription {{ colsql }} AS PackageTariffDescription,
@@ -59,29 +59,31 @@ transformed AS (
         pd.TotalWeight AS PackageTotalWeight,
         pd.shipper {{ colsql }} AS PackageShipper,
         pd.PackageValue AS PackageValue,
-        pd.DateFirstSeen AS PackageFirstSeenDateTime,
-        c.CustomerAccountNumber,
-        c.CustomerAgentPrefix,
+        CONVERT(DATETIME, 
+    CONVERT(VARCHAR(10), pd.DateFirstSeen, 120) + ' ' + 
+    CONVERT(VARCHAR(8), pd.TimeFirstSeen, 108) AS PackageFirstSeenDateTime,
+        pd.AccountNumbers AS CustomerAccountNumber,
+        pd.agent_prefix AS CustomerAgentPrefix,
         pd.location_id AS PackageLocationLastSeenId,
         LEFT(pd.notes, 255) {{ colsql }} AS PackageNotes,
-        '' AS PackagePackedInShipment,
+        packed_in_shipment AS PackagePackedInShipment,
         CASE
-            WHEN pd.package_status = 'Delivered' THEN 'Delivered'
-            WHEN pd.package_status = 'Scheduled for Delivery' THEN 'ScheduledForDelivery'
-            WHEN pd.package_status = 'Received At Warehouse' THEN 'ReceivedWareHouse'
-            WHEN pd.package_status = 'Scanned at Warehouse' THEN 'ScannedWareHouse'
-            WHEN pd.package_status = 'Detained at Customs' THEN 'Detained'
-            WHEN pd.package_status = 'Delivery Cancelled' THEN 'DeliveryCancelled'
-            WHEN pd.package_status = 'In Transit' THEN 'Intransit'
-            WHEN pd.package_status = 'Returned to Warehouse' THEN 'Returned'
-            WHEN pd.package_status = 'Out for Delivery' THEN 'OutForDelivery'
-            WHEN pd.package_status = 'Received from shipper' THEN 'Received'
-            WHEN pd.package_status = 'Released from Customs' THEN 'Released'
+            WHEN pd.PackageStatus = 'Delivered' THEN 'Delivered'
+            WHEN pd.PackageStatus LIKE '%Scheduled for Delivery%' THEN 'ScheduledForDelivery'
+            WHEN pd.PackageStatus = 'Received At Warehouse' THEN 'ReceivedWareHouse'
+            WHEN pd.PackageStatus = 'Scanned at Warehouse' THEN 'ScannedWareHouse'
+            WHEN pd.PackageStatus = 'Detained at Customs' THEN 'Detained'
+            WHEN pd.PackageStatus = 'Delivery Cancelled' THEN 'DeliveryCancelled'
+            WHEN pd.PackageStatus LIKE '%Transit%' THEN 'In Transit'
+            WHEN pd.PackageStatus = 'Returned to Warehouse' THEN 'Returned'
+            WHEN pd.PackageStatus = 'Out for Delivery' THEN 'OutForDelivery'
+            WHEN pd.PackageStatus = 'Received from shipper' THEN 'Received'
+            WHEN pd.PackageStatus = 'Released from Customs' THEN 'Released'
             ELSE 'Received'
         END {{ colsql }} AS PackageActualStatusName,
-        SUBSTRING(pd.package_number, 5, 12) {{ colsql }} AS PackageAirwayBillNumber,
+        SUBSTRING(pd.PackageNumber, 5, 12) {{ colsql }} AS PackageAirwayBillNumber,
         CASE
-            WHEN LEFT(pd.package_number, 3) = 'OCE' THEN 1
+            WHEN LEFT(pd.PackageNumber, 3) = 'OCE' THEN 1
             ELSE 0
         END AS PackageOcean,
         pd.created_at AS PackageCreationDate,
@@ -106,7 +108,7 @@ transformed AS (
             WHEN c.CustomerAvailableSaturday = 1 THEN 1
             ELSE 0
         END AS AvailableSaturdayFlag
-    FROM source s
+    FROM list_of_existing_package_numbers s
     JOIN package_data pd ON pd.package_number {{ colsql }} = s.PackageNumber
     JOIN customer_data c 
         ON pd.account_number = c.CustomerAccountNumber 
